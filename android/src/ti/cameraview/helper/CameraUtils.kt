665d4a86903eac3a03166ef788673d5d03ab354e
Prashant Saini <prashant10103497@gmail.com>
2020-06-07 06:36:49 +0530

    chore: redesign constants class

--- android/src/ti/cameraview/helper/CameraUtils.kt
+++ android/src/ti/cameraview/helper/CameraUtils.kt
@@ -6,7 +6,7 @@ import org.appcelerator.titanium.proxy.TiViewProxy
 class CameraUtils {
     fun fireMoveEvent(tiViewProxy: TiViewProxy, imagePath: String) {
         val eventData = KrollDict()
-        eventData[Defaults.Events.Image.EVENT_PROPERTY_IMAGE_PATH] = imagePath
-        tiViewProxy.fireEvent(Defaults.Events.Image.EVENT_NAME, eventData)
+        eventData[Defaults.Events.Image.PROPERTY_IMAGE_PATH] = imagePath
+        tiViewProxy.fireEvent(Defaults.Events.Image.NAME, eventData)
     }
 }
--- android/src/ti/cameraview/helper/Defaults.kt
+++ android/src/ti/cameraview/helper/Defaults.kt
@@ -1,15 +1,17 @@
 package ti.cameraview.helper
 
 object Defaults {
-    // properties to be exposed through module
-    const val PROPERTY_TORCH_MODE = "torchMode"
-    const val PROPERTY_FLASH_MODE = "flashMode"
+    object Properties {
+        // properties to be exposed through module
+        const val TORCH_MODE = "torchMode"
+        const val FLASH_MODE = "flashMode"
+    }
 
     // events and their properties to be exposed through module
     object Events {
         object Image {
-            const val EVENT_NAME = "image"
-            const val EVENT_PROPERTY_IMAGE_PATH = "imagePath"
+            const val NAME = "image"
+            const val PROPERTY_IMAGE_PATH = "imagePath"
         }
     }
 }
