ee4e2dc0c0e3890202f115ed12b40017f1104069
Prashant Saini <prashant10103497@gmail.com>
2020-06-09 20:52:00 +0530

    chore: repositioned variables to define default variable at the top in its group

--- android/src/ti/cameraview/constant/Defaults.kt
+++ android/src/ti/cameraview/constant/Defaults.kt
@@ -1,11 +1,13 @@
 /*
  * Default values to be applied on camera-view
+ * The first defined property in groups is the default one
  */
 
 package ti.cameraview.constant
 
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.TorchState
 import androidx.camera.view.PreviewView
 
 object Defaults {
@@ -14,24 +16,24 @@ object Defaults {
     const val ASPECT_RATIO_4_3 = AspectRatio.RATIO_4_3
     const val ASPECT_RATIO_16_9 = AspectRatio.RATIO_16_9
 
-    const val TORCH_MODE_ON = true
-    const val TORCH_MODE_OFF = false
+    const val TORCH_MODE_OFF = TorchState.OFF
+    const val TORCH_MODE_ON = TorchState.ON
 
     const val FLASH_MODE_AUTO = ImageCapture.FLASH_MODE_AUTO
     const val FLASH_MODE_ON = ImageCapture.FLASH_MODE_ON
     const val FLASH_MODE_OFF = ImageCapture.FLASH_MODE_OFF
 
-    @JvmField val SCALE_TYPE_FIT_CENTER = PreviewView.ScaleType.FIT_CENTER
-    @JvmField val SCALE_TYPE_FILL_CENTER = PreviewView.ScaleType.FILL_CENTER
-    @JvmField val SCALE_TYPE_FIT_START = PreviewView.ScaleType.FIT_START
-    @JvmField val SCALE_TYPE_FILL_START = PreviewView.ScaleType.FILL_START
-    @JvmField val SCALE_TYPE_FIT_END = PreviewView.ScaleType.FIT_END
-    @JvmField val SCALE_TYPE_FILL_END = PreviewView.ScaleType.FILL_END
+    const val SCALE_TYPE_FIT_CENTER = 4     // PreviewView.ScaleType.FIT_CENTER
+    const val SCALE_TYPE_FILL_START = 0     // PreviewView.ScaleType.FILL_START
+    const val SCALE_TYPE_FILL_CENTER = 1    // PreviewView.ScaleType.FILL_CENTER
+    const val SCALE_TYPE_FILL_END = 2       // PreviewView.ScaleType.FILL_END
+    const val SCALE_TYPE_FIT_START = 3      // PreviewView.ScaleType.FIT_START
+    const val SCALE_TYPE_FIT_END = 5        // PreviewView.ScaleType.FIT_END
 
-    const val FOCUS_MODE_AUTO = 0     // auto-focus
-    const val FOCUS_MODE_TAP = 1      // tap-to-focus
+    const val FOCUS_MODE_AUTO = 0           // auto-focus
+    const val FOCUS_MODE_TAP = 1            // tap-to-focus
 
-    const val RESUME_AUTO_FOCUS_ON_AFTER_FOCUS_MODE_TAP = true
-    const val RESUME_AUTO_FOCUS_OFF_AFTER_FOCUS_MODE_TAP = false
+    const val RESUME_AUTO_FOCUS_AFTER_FOCUS_MODE_TAP = true
+
     const val RESUME_AUTO_FOCUS_TIME_AFTER_FOCUS_MODE_TAP = 5    // in seconds
 }
--- android/src/ti/cameraview/constant/Properties.kt
+++ android/src/ti/cameraview/constant/Properties.kt
@@ -8,9 +8,9 @@ object Properties {
     // properties to be exposed through module
     const val TORCH_MODE = "torchMode"
     const val FLASH_MODE = "flashMode"
-    const val FOCUS_MODE = "focusMode"
     const val ASPECT_RATIO = "aspectRatio"
     const val SCALE_TYPE = "scaleType"
+    const val FOCUS_MODE = "focusMode"
     const val RESUME_AUTO_FOCUS = "resumeAutoFocus"
     const val AUTO_FOCUS_RESUME_TIME = "autoFocusResumeTime"
 }
